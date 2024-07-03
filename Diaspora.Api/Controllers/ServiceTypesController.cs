bc782356be9092ed9ac72563fe062c6dd7ef4851
Oscar Mauricio Benavidez Suarez <obenavidez@gmail.com>
2024-07-03 02:58:49 -0500

    Despliegue

--- Diaspora.Api/Controllers/ServiceTypesController.cs
+++ Diaspora.Api/Controllers/ServiceTypesController.cs
@@ -14,7 +14,7 @@ namespace Diaspora.Api.Controllers
         [HttpGet]
         public IEnumerable<string> Get()
         {
-            return new string[] { "value1", "value2", "value3", "value4" };
+            return new string[] { "value1", "value2", "value3", "value4", "value5", "value6" };
         }
 
         // GET api/<ServiceTypesController>/5
