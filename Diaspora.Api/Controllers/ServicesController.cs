0bf7f9031e6aabaaf5b6b52955c24c8ac52b90ba
Oscar Mauricio Benavidez Suarez <obenavidez@gmail.com>
2024-09-17 09:46:33 -0500

    US_75: Agregar Value3 y Value4 al endpoint de services y crear tabla userNew

--- Diaspora.Api/Controllers/ServicesController.cs
+++ Diaspora.Api/Controllers/ServicesController.cs
@@ -28,7 +28,7 @@ namespace Diaspora.Api.Controllers
         [HttpGet]
         public IEnumerable<string> Get()
         {
-            return new string[] { "value1", "value2" };
+            return new string[] { "value1", "value2", "value3", "value4" };
         }
 
         [HttpPost("cheapestprice")]
